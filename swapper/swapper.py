47ad6161e7f3cef04b3549b6a7df25edcccd5725
“Darice <daricesousa@gmail.com>
2022-10-03 18:56:01 -0300

    quantity of producers, consumers, e queues

--- swapper/swapper.py
+++ swapper/swapper.py
@@ -1,10 +1,13 @@
 import Pyro4
 import threading
 
+from more_itertools import quantify
+
 @Pyro4.expose
 class Swapper(object):
   __queues = {}
   __consumers = []
+  __quantity_producers = 0
 
   def __create_queue(self, rule: str) -> None:
     if (self.__queues.get(rule) is None):
@@ -47,6 +50,26 @@ class Swapper(object):
   def new_consumer(self, rule: str) -> None:
     self.__consumers.insert(0, rule)
     print(self.__consumers)
+
+  def new_producer(self) -> None:
+    self.__quantity_producers += 1
+
+  def quantity_producers(self) -> int:
+    return self.__quantity_producers
+
+  def quantity_consumers(self) -> int:
+    return len(self.__consumers)
+
+  def queues(self) -> list:
+    keys = []
+    for queue in self.__queues: 
+      for key, _ in queue:
+        keys.append(key)
+    return keys
+
+  def len_queues(self, rule) -> int:
+    return len(self.__queues[rule]["value"])
+  
 
 daemon = Pyro4.Daemon()
 uri = daemon.register(Swapper)
